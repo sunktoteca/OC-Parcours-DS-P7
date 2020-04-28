18250accad9e15b67b7cb13c5886c02a9f94253e
sunktoteca <aiandi.laure@gmail.com>
2020-04-28 11:36:03 +0200

    changement du repertoire de travail - pas sur que cela soit reellement utile

--- dashboard/www/laure_eu_pythonanywhere_com_wsgi.py
+++ dashboard/www/laure_eu_pythonanywhere_com_wsgi.py
@@ -1,19 +1,26 @@
 # This file contains the WSGI configuration required to serve up your
-# web application at http://<your-username>.pythonanywhere.com/
+# web application at http://LaureP7.eu.pythonanywhere.com/
 # It works by setting the variable 'application' to a WSGI handler of some
 # description.
 #
-# The below has been auto-generated for your Flask project
+
+# +++++++++++ GENERAL DEBUGGING TIPS +++++++++++
+# getting imports and sys.path right can be fiddly!
+# We've tried to collect some general tips here:
+# https://help.pythonanywhere.com/pages/DebuggingImportError
+
+
 
 import sys
 
 # add your project directory to the sys.path
-project_home = '/home/Laure/P7'
+project_home = '/home/LaureP7/p7api'
 if project_home not in sys.path:
-    sys.path = [project_home] + sys.path
+    sys.path.append(project_home)
+     #sys.path = [project_home] + sys.path
 
 # import flask app but need to call it "application" for WSGI to work
 # from flask_app import app as application  # noqa
 from flask_app import app
 application = app.server
-# from flask_app import server as application
+
